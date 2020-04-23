7fcdc6fe85757d32ca7a37d49c8bf727e46c72c6
youyu.chen <youyu.chen@1mu.club>
2020-04-23 18:26:44 +0800

    fix: effect

--- Assets/Scripts/Pig.cs
+++ Assets/Scripts/Pig.cs
@@ -4,12 +4,14 @@ using UnityEngine;
 
 public class Pig : MonoBehaviour {
 
-	public float maxSpeed = 10;
-	public float minSpeed = 5;
+	public float maxSpeed = 6;
+	public float minSpeed = 3;
 
 	private SpriteRenderer render;
 
 	public Sprite hurtPig;
+
+	public GameObject boom;
 
 	private void Awake() {
 		render = GetComponent<SpriteRenderer>();	
@@ -18,9 +20,14 @@ public class Pig : MonoBehaviour {
 	private void OnCollisionEnter2D(Collision2D other) {
 		print("当前速度为" + other.relativeVelocity.magnitude);
 		if(other.relativeVelocity.magnitude > maxSpeed){
-			Destroy(gameObject);
+			Dead();
 		} else if(other.relativeVelocity.magnitude > minSpeed && other.relativeVelocity.magnitude < maxSpeed){
 			render.sprite = hurtPig;
 		}
+	}
+
+	void Dead(){
+		Destroy(gameObject);
+		Instantiate(boom, transform.position, Quaternion.identity);
 	}
 }
